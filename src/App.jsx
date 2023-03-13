6d70a7cf84d5f9c01a819c0333d9f5f5e2f31092
nishchit <nishchitpatel84@gmail.com>
2023-03-14 04:29:19 +0530

    added text below

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,27 @@ import Html5QrcodePlugin from "./Qr";
 import Config from "./config";
 function App() {
   const [data,setData] = useState(null)
+  const [results,setResult] = useState(null)
   const onNewScanResult = (decodedText, decodedResult) => {
     // handle decoded results here
     console.log(decodedText)
-    console.log(decodedResult)
+    setData(JSON.parse(decodedText))
 
+    axios.post(Config.baseUrl+"/verify",option).then((res) => {
+      console.log(res);
+      setResult(res)
+    });
   };
   useEffect(() => {
     let option = {
-      publicKey: "0x85c3E62be3Fedc174b6DCc3F7c65e1b23eFA97EF",
-      contractAddress: "0x1f8f2e94cfe82a31bb880d4ca212086737157ac7",
-      tokenId: "1094",
-      hash: "0x50b2c43fd39106bafbba0da34fc430e1f91e3c96ea2acee2bc34119f92b37750",
+      publicKey: data.publicKey,
+      contractAddress: data.contractAddress,
+      tokenId: data.tokenId,
+      hash: data.signMessage,
       signMessage:
-        "0x645cf8784f6d0ff9b714d75ae5eac3dacfc6f1ff2a7e42bf77f1e2823404c0a77a40916fa281df9daec77ce655c12fa1adefba67eddbe14066ea129034388c8e1b",
+        data.hash,
     };
-    axios.post( Config.baseUrl+"/verify",option).then((res) => {
-      console.log(res);
-    });
+   
   }, []);
 
   return (
@@ -36,7 +39,7 @@ function App() {
         ></Html5QrcodePlugin>
       </h1>
       <p>
-        {data != null? data.publicKey:''}
+        {results ? <p>Verification: ${results.verification}</p>:<p>Verification: ${results.verification}</p>}
       </p>
     </div>
   );
