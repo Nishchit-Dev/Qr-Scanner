c546d589bbb434a9aca374184e339dc4fbde1d9c
nishchit <nishchitpatel84@gmail.com>
2023-03-14 14:02:10 +0530

    flag for verification

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import axios from "axios";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import Html5QrcodePlugin from "./Qr";
 import Config from "./config";
 function App() {
-  const [data,setData] = useState(null)
-  const [results,setResult] = useState(null)
+  const [data, setData] = useState(null);
+  const [results, setResult] = useState(null);
   const onNewScanResult = (decodedText, decodedResult) => {
     // handle decoded results here
-    if(typeof decodedResult == "object"){
+    if (typeof decodedResult == "object") {
+      console.log(JSON.parse(decodedText).publicKey);
+      setData(JSON.parse(decodedText));
 
-    console.log(JSON.parse(decodedText).publicKey)
-    setData(JSON.parse(decodedText))
-
-    axios.post(Config.baseUrl+"/verify",JSON.parse(decodedText)).then((res) => {
-      console.log(res);
-      setResult(res.data)
-    });
-  }
-
+      axios
+        .post(Config.baseUrl + "/verify", JSON.parse(decodedText))
+        .then((res) => {
+          console.log(res);
+          setResult(res.data);
+        });
+    }
   };
-  
 
   return (
     <div className="App">
@@ -32,9 +31,7 @@ function App() {
           qrCodeSuccessCallback={onNewScanResult}
         ></Html5QrcodePlugin>
       </h1>
-      <p>
-        {results != null ? <p>Verification: Successful</p>:<p>Verification: Failed</p>}
-      </p>
+      <p>{results ? <p>Verification: {results}</p> : ""}</p>
     </div>
   );
 }
